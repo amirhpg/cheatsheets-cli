0f28dc3a52eeda6b8d1267b7839e08c393ea9ac3
amirhpg <amirhpg.wm@gmail.com>
2019-01-13 13:00:47 +0330

    download operation completed maybe needs a tweak in other availble cheetsheets part

--- cheatsheets.py
+++ cheatsheets.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.7
+import sys
+import os.path
 import click
 import requests
 from bs4 import BeautifulSoup
@@ -8,9 +10,9 @@ import re
 
 
 @click.command()
-@click.option('--count', default="hi", help='number of greetings')
+@click.option('--link', default=1, help='which cheatsheet link to download')
 @click.argument('name')
-def main(count, name):
+def main(link, name):
     website = "http://overapi.com/"
     dlpage = website+name
     content = urllib.request.urlopen(dlpage).read()
@@ -18,15 +20,90 @@ def main(count, name):
     title = soup.title.string
     a = soup.find_all("a", href=True)
     i = 1
+    othercheatsheetsindicator = 0
+    downloadcompleteindicator = 0
     for value in a:
         if value['href'].endswith(".pdf"):
             p = re.compile('^(?!http://.*$).*')
             if p.match(value['href']):
-                print("[ "+str(i)+" ] http://overapi.com"+value['href'])
-                i+=1
+                fileurl = "http://overapi.com"+value['href']
+                filename = value['href'].rsplit('/')[-1]
+                if os.path.isfile(filename):
+                    print(filename+" already exist! ☉")
+                    i+=1
+                else:
+                    if i == link :
+                         print("[ "+str(i)+" ] "+fileurl+" [Downloading] ⌛")
+                         download(fileurl,filename)
+                         if os.path.isfile(filename):
+                             print("\nDownload completed successfully ✓")
+                             downloadcompleteindicator+=1
+                         else:
+                            print("error ✗")
+
+                         i+=1
+                    else:
+                        if downloadcompleteindicator != 0:
+                            if othercheatsheetsindicator == 0:
+                                print("\n")
+                                print("+----------------------------------------------------+")
+                                print("|            Other availble cheetsheets              |")
+                                print("+----------------------------------------------------+")
+                                othercheatsheetsindicator+=1;
+
+                            print("[ "+str(i)+" ] "+fileurl)
+                            
+                        i+=1
             else:
-                print("["+str(i)+"] "+ value['href'])
-                i+=1
+                fileurl = value['href']
+                filename = value['href'].rsplit('/')[-1]
+                if os.path.isfile(filename):
+                    print(filename+" already exist! ☉")
+                    i+=1
+                else:
+                    if i == link :
+                         print("[ "+str(i)+" ] "+fileurl+" [Downloading] ⌛")
+                         download(fileurl,filename)
+                         if os.path.isfile(filename):
+                             print("\nDownload completed successfully ✓")
+                             downloadcompleteindicator+=1
+                         else:
+                            print("error ✗")
+                         i+=1
+                    else:
+                        if downloadcompleteindicator != 0:
+                            if othercheatsheetsindicator == 0:
+                                print("\n")
+                                print("+----------------------------------------------------+")
+                                print("|            Other availble cheetsheets              |")
+                                print("+----------------------------------------------------+")
+                                othercheatsheetsindicator+=1;
+
+                            print("[ "+str(i)+" ] "+fileurl)
+                        
+                        i+=1
+
+
+
+def download(url, filename):
+    with open(filename, 'wb') as f:
+        response = requests.get(url, stream=True)
+        total = response.headers.get('content-length')
+
+        if total is None:
+            f.write(response.content)
+        else:
+            downloaded = 0
+            total = int(total)
+            for data in response.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
+                downloaded += len(data)
+                f.write(data)
+                done = int(50*downloaded/total)
+                sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50-done)))
+                sys.stdout.flush()
+    sys.stdout.write('\n')
+
+
 # TODO: user should can download the links 
 # TODO: show all avalible cheetsheets
 # TODO: add other sources - optional-
